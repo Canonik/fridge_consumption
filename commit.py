5ebe3ea2ed82651a1942aea029efb0569fb5d609
Canonik <alessandro006@icloud.com>
2025-11-29 13:14:58 +0100

    Add .gitignore

--- commit.py
+++ commit.py
@@ -1,10 +1,8 @@
 import pandas as pd
 
-# Load the dataframe
-df = pd.read_csv('train.xlsx - train.csv')
+df = pd.read_csv("data/test.csv")
 
-# Display the first 5 rows
+
 print(df.head().to_markdown(index=False, numalign="left", stralign="left"))
 
-# Print the column names and their data types
 print(df.info())
