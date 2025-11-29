7825bdcece0860036df75e64b029132fb7f3ceff
Canonik <alessandro006@icloud.com>
2025-11-29 13:11:05 +0100

    data

--- commit.py
+++ commit.py
@@ -1,1 +1,10 @@
-commit
+import pandas as pd
+
+# Load the dataframe
+df = pd.read_csv('train.xlsx - train.csv')
+
+# Display the first 5 rows
+print(df.head().to_markdown(index=False, numalign="left", stralign="left"))
+
+# Print the column names and their data types
+print(df.info())
